43ce6b08d167dd36a856173f37eac557be422515
Ye Lu <info@fais.world>
2024-11-29 14:39:27 +0200

    Update app.py
    listen to port 8080

--- src/subproject/app.py
+++ src/subproject/app.py
@@ -1,10 +1,12 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Welcome to the Kvitka Subproject!"})
+    return "Hello, World!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
